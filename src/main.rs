9b88a62e23a445f79fe8dc6a47ae775afc509fcb
nabindhimal <nabindhimal288@gmail.com>
2024-02-16 13:18:33 +0545

    password generator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 use colored::*;
 use rand::Rng;
-use rand::seq::SliceRandom;  // Import the necessary trait
+use rand::seq::SliceRandom;
 
 fn main() {
     let text = "Welcome to the Rust Password Generator!";
@@ -23,40 +23,35 @@ fn main() {
 
 fn generate_password(length: usize) -> String {
     let mut rng = rand::thread_rng();
-    let alphanumeric_password: String = (0..length)
+    let alphanumeric_length = length * 3 / 4;
+    let additional_length = length - alphanumeric_length;
+
+    let alphanumeric_password: String = (0..alphanumeric_length)
         .map(|_| rng.sample(rand::distributions::Alphanumeric) as char)
         .collect();
 
     let additional_characters: &str = "!@#$%^&*";
     let additional_numbers: &str = "0123456789";
-    let additional_length = length / 4; // You can adjust this ratio as needed
 
-    let additional_chars: Vec<char> = (0..additional_length)
+    let additional_chars: String = (0..additional_length / 2)
         .map(|_| {
             let index = rng.gen_range(0..additional_characters.len());
             additional_characters.chars().nth(index).unwrap()
         })
         .collect();
 
-    let mut additional_numbers: Vec<char> = (0..additional_length)
+    let additional_numbers: String = (0..additional_length / 2)
         .map(|_| {
             let index = rng.gen_range(0..additional_numbers.len());
             additional_numbers.chars().nth(index).unwrap()
         })
         .collect();
 
-    let mut combined_chars = additional_chars;
-    combined_chars.append(&mut additional_numbers);
+    let combined_chars = format!("{}{}{}", alphanumeric_password, additional_chars, additional_numbers);
 
-    combined_chars.shuffle(&mut rng);
+    let mut password_chars: Vec<char> = combined_chars.chars().collect();
+    password_chars.shuffle(&mut rng);
 
-    let final_password: String = alphanumeric_password
-        .chars()
-        .zip(combined_chars.iter())
-        .flat_map(|(a, b)| vec![a, *b])
-        .collect();
-
-    final_password
+    password_chars.into_iter().collect()
 }
-
 
